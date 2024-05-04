739348527cef8d1d0d8e5d19de3359ddf88a0e00
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-05-04 17:21:48 -0500

    signup form created with uncontrolled component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,46 @@
+import { useOptimistic, useState, useRef } from "react";
+//add optimistic message to make more responsive maybe?
+
+//username, password, confirm password
+
 function Signup() {
+	//form data, setformdata
+	//empty
+	//typing
+	//submitting
+	//success/error
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		for (let [key, value] of formData.entries()) {
+			console.log(`${key}: ${value}`, "this is key, value");
+		}
+
+		fetch("http://localhost:3000/signup", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ formData }),
+		})
+			.then((response) => response.text())
+			.then((data) => {
+				console.log(data, "this is data");
+			});
+	};
+
+	//handlechange event
 	return (
 		<>
-			<div>signup test</div>
+			<form action="/signup" onSubmit={handleSubmit}>
+				<label>Username:</label>
+				<input type="text" name="name" />
+				<button type="submit"></button>
+			</form>
 		</>
 	);
 }
 
+{
+	/* <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required></input> */
+}
 export default Signup;
